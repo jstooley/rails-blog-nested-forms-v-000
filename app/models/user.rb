34ad30e0b2961ecf936d3896b3e113ea2466c9b7
Justin Tooley <jstooley823@gmail.com>
2018-01-02 22:59:58 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
-  accepts_nested_attributes_for :posts, reject_if: proc { |attributes| attributes['title'].blank? }
+  accepts_nested_attributes_for :posts
   
   validates_uniqueness_of :name
   validates_presence_of :name
